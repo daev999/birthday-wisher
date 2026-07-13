8a038766c69bad0e548e6327a76d8c12e696fcef
David Ekeocha <davidekeocha36@gmail.com>
2026-07-13 18:21:37 +0100

    Finish automated birthday wisher project

--- main.py
+++ main.py
@@ -1,69 +1,40 @@
 ##################### Hard Starting Project ######################
 import datetime as dt
 import pandas
+import random
+import smtplib
+from email.message import EmailMessage
+
 
 now = dt.datetime.now()
 
 today_month = now.month
 today_day = now.day
-
 birthdays = pandas.read_csv("birthdays.csv")
 
+birthdays_dict = {}
+
+MY_EMAIL = "your_email"
+MY_PASSWORD = "your_password"
+
 for index, row in birthdays.iterrows():
-    pass
+    birthdays_dict[row["month"], row["day"]] = row
 
-# 1. Update the birthdays.csv with your friends & family's details.
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+if (today_month, today_day) in birthdays_dict:
+    celebrant = birthdays_dict[(today_month, today_day)]
+    letters = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+    chosen_letter = random.choice(letters)
+    with open (chosen_letter, "r") as file:
+        letter = file.read()
+        letter = letter.replace("[NAME]", celebrant["name"])
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+    message = EmailMessage()
+    message["Subject"] = "Happy Birthday!"
+    message["From"] = MY_EMAIL
+    message["To"] = celebrant["email"]
+    message.set_content(letter)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-
-# Goal
-# │
-# └── Send a birthday email automatically.
-#
-# ────────────────────────────
-#
-# Information
-#
-# • Today's month and day
-# • Birthday list
-# • Names
-# • Email addresses
-# • Birthday letter templates
-#
-# ────────────────────────────
-#
-# Decision
-#
-# Is today someone's birthday?
-#
-#         │
-#    No ──┘ End
-#
-#         │
-#        Yes
-#
-# ────────────────────────────
-#
-# Actions
-#
-# • Pick a random letter
-# • Replace [NAME]
-# • Create the email
-# • Connect to Gmail
-# • Log in
-# • Send the email
-# • End
-
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.send_message(message)
